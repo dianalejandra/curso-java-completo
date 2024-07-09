104f52f8285a1c6ff563c029cf20539ddae7776a
dianalejandra <diana.abate@gmail.com>
2024-07-09 15:30:11 -0300

    working with variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,15 @@ public class HelloWorld {
     public static void main(String[] args) {
         String greeting = "Hello and welcome!" ;
         System.out.println(greeting);
+        System.out.println("greeting.t: " + greeting.toUpperCase());
+
+        int number = 10;
+        boolean value = true;
+        int otherNumber = 0;
+        if (value) {
+            otherNumber = 15;
+            System.out.println("number = " + number);
+        }
+        System.out.println("other number = " + otherNumber);
     }
 }
