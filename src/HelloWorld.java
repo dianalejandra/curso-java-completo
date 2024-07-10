194a9e860d07092f28ccbad4c356273300dbbdbc
dianalejandra <diana.abate@gmail.com>
2024-07-10 15:55:55 -0300

    working on how variables are declared and initialized, checking good practices

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,13 +4,24 @@ public class HelloWorld {
         System.out.println(greeting);
         System.out.println("greeting.t: " + greeting.toUpperCase());
 
-        int number = 10;
+        int number = 11;
         boolean value = true;
-        int otherNumber = 0;
+        int number2 = 0;
         if (value) {
-            otherNumber = 15;
             System.out.println("number = " + number);
+            number2 = 15;
         }
-        System.out.println("other number = " + otherNumber);
+        System.out.println("other number = " + number2);
+
+        var number3 = "15";
+
+        String name;
+
+        name = "Diana";
+
+        if (number > 10) {
+            name = "Alejandra";
+        }
+        System.out.println("name = " + name);
     }
 }
