3f961eadd61314d53e3f2fab7cf7bae3d2b19c92
dianalejandra <diana.abate@gmail.com>
2024-07-10 17:54:00 -0300

    adding more examples of numeric primitives

---
+++ src/FloatPrimitives.java
@@ -0,0 +1,7 @@
+public class FloatPrimitives {
+    public static void main(String[] args) {
+        /* MORE NUMERIC PRIMITIVES PRACTICE*/
+        float realFloat = 0.00000000015f; // 1.5e-10f;
+        System.out.println("realFloat = " + realFloat);
+    }
+}
