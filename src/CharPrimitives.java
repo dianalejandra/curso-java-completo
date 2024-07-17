0aee6db9673cc0a000d1c31e6e173b1474268177
dianalejandra <diana.abate@gmail.com>
2024-07-17 16:16:15 -0300

    adding more explanations on primitives

--- src/CharPrimitives.java
+++ src/CharPrimitives.java
@@ -1,6 +1,9 @@
 public class CharPrimitives {
     public static void main(String[] args) {
-        char character = '1';
+        char character = '\u0040';
+        char decimal = 64;
         System.out.println("character = " + character);
+        System.out.println("decimal = " + decimal);
+        System.out.println("decimal = character: " + (decimal == character));
     }
 }
