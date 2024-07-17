cb5f9347a6be868c298caebd5bbf23c8257d6439
dianalejandra <diana.abate@gmail.com>
2024-07-17 17:02:45 -0300

    adding more explanations on primitives

--- src/Primitives/BooleanPrimitives.java
+++ src/Primitives/BooleanPrimitives.java
@@ -3,8 +3,20 @@ package Primitives;
 public class BooleanPrimitives {
     public static void main(String[] args) {
 
-        boolean logicData = Boolean.FALSE;
+        boolean logicData = true;
         System.out.println("logicData = " + logicData);
+
+        double d = 98765.43e-3; // 98.76543
+        System.out.println("d = " + d);
+        float f = 12345e2f; // 123.45
+        System.out.println("f = " + f);
+
+        logicData = d < f;
+        System.out.println("logicData = " + logicData);
+
+        boolean isEqual = (3-3 == 1);
+        System.out.println("isEqual = " + isEqual);
+
 
     }
 }
