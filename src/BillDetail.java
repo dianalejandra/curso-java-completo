7b944e92ec1966bfee784f9d83bc6b58ab5176bb
Diana Abate <diana.abate@gmail.com>
2024-08-08 16:03:34 -0300

    adding the description of the assignment

--- src/BillDetail.java
+++ src/BillDetail.java
@@ -1,3 +1,20 @@
+/**
+ The task is to create a new class with the main method called InvoiceDetail. You need to develop a program that receives invoice data using the Scanner class as follows:
+
+ Receive the invoice name or description. Use the nextLine() method to obtain the name of the invoice (including spaces).
+ Receive 2 product prices of type double. Use the nextDouble() method to obtain prices with decimals (,).
+ Calculate the total by summing both prices and adding a 19% tax.
+ You are required to display the following information in a single string:
+
+ - Invoice name
+ - Gross total (before tax)
+ - Tax amount
+ - Net total (including tax)
+
+ For example, the result could be something like this:
+
+ “The office supply invoice has a gross total of $134.78, with a tax of $25.6082, resulting in a net amount after tax of $160.3882”.
+ * **/
 import java.util.Scanner;
 public class BillDetail {
     public static void main(String[] args) {
@@ -13,11 +30,11 @@ public class BillDetail {
         System.out.println("Tell us the second price:");
         double priceTwo = inputPriceTwo.nextDouble();
 
-        double totalBills = priceOne + priceTwo;
+        double grossTotal = priceOne + priceTwo;
         double percentage = 0.19;
-        double taxTotal = totalBills * percentage;
-        double totalWithTaxes = totalBills + taxTotal ;
+        double taxTotal = grossTotal * percentage;
+        double totalWithTaxes = grossTotal + taxTotal ;
 
-        System.out.println("The invoice " + billName +" has a gross total of " + totalBills + ", with a tax total of " + taxTotal + " and the amount after tax is "  + totalWithTaxes );
+        System.out.println("The invoice " + billName +" has a gross total of " + grossTotal + ", with a tax total of " + taxTotal + " and the amount after tax is "  + totalWithTaxes );
     }
 }
