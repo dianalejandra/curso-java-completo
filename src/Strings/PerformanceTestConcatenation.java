eda929eadcec11094dacd85629c90199e0d26e5c
Diana Abate <diana.abate@gmail.com>
2024-08-09 16:50:46 -0300

    working with strings in Java

--- src/Strings/PerformanceTestConcatenation.java
+++ src/Strings/PerformanceTestConcatenation.java
@@ -2,8 +2,21 @@ package Strings;
 
 public class PerformanceTestConcatenation {
     public static void main(String[] args) {
-        
+        /* We  will test and check how much time it takes to concatenate with '+' operator, versus concat() method, versus StringBuilder */
 
+        String a = "a";
+        String b = "b";
+        String c = a;
+
+        /* We take 'start' time and an 'end' time  */
+        long start = System.currentTimeMillis();
+
+        for(int i=0; i< 500 ;i++){
+            
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
 
     }
 }
