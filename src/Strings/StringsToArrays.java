19c9c4efac86904465c83ce1cc22346c6b67c565
Diana Abate <diana.abate@gmail.com>
2024-08-14 17:06:08 -0300

    adding example of StringsToArrays

---
+++ src/Strings/StringsToArrays.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class StringsToArrays {
+    public static void main(String[] args) {
+        /* Converting Strings to Arrays */
+        String tongueTwisters = "tongue twisters";
+    }
+}
