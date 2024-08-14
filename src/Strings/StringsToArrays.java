c0de8464d46f983a461bc221bb4b89855fcfc111
Diana Abate <diana.abate@gmail.com>
2024-08-14 17:21:18 -0300

    adding example of StringsToArrays

--- src/Strings/StringsToArrays.java
+++ src/Strings/StringsToArrays.java
@@ -4,5 +4,15 @@ public class StringsToArrays {
     public static void main(String[] args) {
         /* Converting Strings to Arrays */
         String tongueTwisters = "tongue twisters";
+        System.out.println("tongueTwisters.length() = " + tongueTwisters.length());
+        System.out.println("tongueTwisters.toCharArray() = " + tongueTwisters.toCharArray());
+
+        char[] myStringArray = tongueTwisters.toCharArray();
+        int arrayLength = myStringArray.length;
+        System.out.println("the length of the array: " + arrayLength);
+        System.out.println("printed array: ");
+        for(int i = 0; i< arrayLength ;i++){
+            System.out.print(myStringArray[i]);
+        }
     }
 }
