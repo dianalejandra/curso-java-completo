2a5474640fe96b8e8ad824c86917d3ae7f80d23f
Diana Abate <diana.abate@gmail.com>
2024-08-15 12:35:50 -0300

    adding example of StringsToArrays

--- src/Strings/StringsToArrays.java
+++ src/Strings/StringsToArrays.java
@@ -14,5 +14,28 @@ public class StringsToArrays {
         for(int i = 0; i< arrayLength ;i++){
             System.out.print(myStringArray[i]);
         }
+
+        // We want to convert it with the .split and put 't' as a parameter for division
+        System.out.println();
+        System.out.println("\ntongueTwisters = " + tongueTwisters.split("t"));
+
+        String [] arraySplit = tongueTwisters.split("t");
+        int arraySplitLength = arraySplit.length;
+
+        for(int index=0; index< arraySplitLength; index++ ){
+            System.out.println(arraySplit[index]);
+        }
+
+        String file = "some.file.exe";
+        String [] fileArray = file.split("[.]"); // we escape with \\. or [.]
+        int l = fileArray.length;
+        System.out.println();
+        for(int index=0; index< l; index++ ){
+            System.out.println(fileArray[index]);
+        }
+
+        System.out.println("the extension = " + fileArray[l-1]);
+
+
     }
 }
