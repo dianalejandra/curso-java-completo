fb067b6293df2fb6043bff3789a1837539ae3939
dianalejandra <diana.abate@gmail.com>
2024-09-09 17:28:44 -0300

    making an example with a login to practice logical operators

---
+++ src/Operators/LogicalOperatorsLogin.java
@@ -0,0 +1,35 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class LogicalOperatorsLogin {
+    public static void main(String[] args) {
+        String username = "Diana";
+        String password = "123456";
+
+        String username2 = "admin";
+        String password2 = "123456";
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please write your username: ");
+        String user = keyboard.next();
+
+        System.out.println("Write your password: ");
+        String pw = keyboard.next();
+
+        boolean isAuth = false;
+
+        if(username.equals(user) &&  password.equals(pw) || username2.equals(user) && password2.equals(pw)){
+            isAuth = true;
+        } else {
+            System.out.println("User or password are invalid.");
+        }
+
+        if(isAuth){
+            System.out.println("Welcome ".concat(user).concat("!"));
+        } else {
+            System.out.println("I am sorry, you require authentication.");
+        }
+    }
+}
