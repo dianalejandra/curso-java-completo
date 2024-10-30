5bdf901d6cfe124d6547b54831bbe668dc113188
Diana Abate <diana.abate@gmail.com>
2024-10-30 12:05:40 -0300

    adding more examples on switch

--- src/ControlFlow/SwitchCaseSentence.java
+++ src/ControlFlow/SwitchCaseSentence.java
@@ -2,9 +2,22 @@ package ControlFlow;
 
 public class SwitchCaseSentence {
     public static void main(String[] args) {
-        int num = 3;
+        int num = 1;
         switch(num){
-
+            case 0:
+                System.out.println("The number is zero");
+                break;
+            case 1:
+                System.out.println("The number is one");
+                break;
+            case 2:
+                System.out.println("The number is two");
+                break;
+            case 3:
+                System.out.println("The number is three");
+                break;
+            default:
+                System.out.println("Unknown number");
         }
     }
 }
