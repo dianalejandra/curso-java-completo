63b28e0553bbb9afaa9f9bebac903f5ca87e2e54
dianalejandra <diana.abate@gmail.com>
2024-11-01 15:40:53 -0300

    adding more details on for loop example

--- src/ControlFlow/ForLoop.java
+++ src/ControlFlow/ForLoop.java
@@ -26,5 +26,14 @@ public class ForLoop {
             }
             System.out.println("i = " + i);
         }
+
+        System.out.println("------------");
+
+        for (int i = 0; i <=10 ; i++) {
+            if (!(i % 2 ==0)){
+                continue;
+            }
+            System.out.println("i = " + i);
+        }
     }
 }
