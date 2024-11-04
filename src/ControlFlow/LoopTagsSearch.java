bada1152df8e85d250d6117f113d75d4ff32528e
dianalejandra <diana.abate@gmail.com>
2024-11-04 15:17:49 -0300

    working on mor complex control flow examples

--- src/ControlFlow/LoopTagsSearch.java
+++ src/ControlFlow/LoopTagsSearch.java
@@ -1,4 +1,29 @@
 package ControlFlow;
 
 public class LoopTagsSearch {
+    public static void main(String[] args) {
+        // Search for a word in a phrase or sentence. See how many times it appears on the phrase
+        String phrase = "Peter Piper picked a peck of pickled peppers. A peck of pickled peppers Peter Piper picked. If Peter Piper picked a peck of pickled peppers, where’s the peck of pickled peppers Peter Piper picked?";
+
+        String word = "peck";
+
+        int maxWord = word.length();
+        int maxChar = phrase.length() - maxWord;
+        int count = 0;
+        phrase = phrase.toLowerCase();
+
+        search:
+        for (int i = 0; i <= maxChar;) {
+            int k = i;
+            for (int j = 0; j < maxWord; j++) {
+                if (phrase.charAt(k++) != word.charAt(j)) {
+                    i++;
+                    continue search;
+                }
+            }
+            count++;
+            i = i + maxWord;
+        }
+        System.out.println("Found the word '" + word + "' " + count + " times in the phrase");
+    }
 }
