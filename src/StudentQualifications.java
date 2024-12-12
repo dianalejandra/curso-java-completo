85a6a91bf829965d11046aedb002895736e94780
Diana Abate <diana.abate@gmail.com>
2024-12-12 10:51:59 -0300

    continuing the work on the loop assignment

--- src/StudentQualifications.java
+++ src/StudentQualifications.java
@@ -1,21 +1,25 @@
+/*
+Request 20 final grades from students on a scale of 1 to 7, handling decimals in the grades (double). Show the average of grades greater than 5, average of grades below 4, and the number of grades that are 1, as well as the total average.
+
+Help: use a for loop that iterates up to 20 (grades) and within the loop ask for the grades one by one to perform calculations (counters, sums).
+
+Optional: if one of the entered grades is 0, the for loop should exit, and display an error message ending the program.*/
 import java.util.Scanner;
 
 public class StudentQualifications {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        double [] qualifications = new double[20];
 
-        for (int i = 0; i < qualifications.length; i++) {
-            System.out.println("Please enter a qualification: ");
-            qualifications[i] = keyboard.nextDouble();
-        }
+        double grade;
+        int gradeCounter1 = 0;
+        int gradeCounterGreaterThan5 = 0; // the counter for scores greater than 5
+        int gradeCounterLessThan4 = 0; // the counter for scores less than 4
+        double sumGradesGreaterThan5 = 0; // sum of grades that are grater than 5
+        double sumGradesLessThan4 = 0; // sum of grades that are less than 4
+        double totalSum = 0; // total sum of all grades
 
-        double sum = 0;
-        for (int i = 0; i < qualifications.length ; i++) {
-            sum += qualifications[i];
-        }
 
-        double average = sum / qualifications.length;
-        System.out.println("The average qualification is: " + average);
+
+
     }
 }
