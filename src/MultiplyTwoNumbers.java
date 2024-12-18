5ac81a749f4bc5171d0362ea9f9db24a1d093119
Diana Abate <diana.abate@gmail.com>
2024-12-18 11:26:50 -0300

    starting wth new assignment of moltplying numbers

---
+++ src/MultiplyTwoNumbers.java
@@ -0,0 +1,8 @@
+/*
+Ask for two positive or negative integers via the keyboard and multiply them, but without using the multiplication symbol (*).
+You can use a for loop to perform the multiplication and account for unary operations, where minus times minus is positive.*/
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        
+    }
+}
