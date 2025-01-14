d8cbe6af969417b1f7fcf74e54f052dd4df0db40
Diana Abate <diana.abate@gmail.com>
2025-01-14 10:23:29 -0300

    explanation of passing values versus passing references

--- src/ValueOrReferenceArguments/ValueArguments.java
+++ src/ValueOrReferenceArguments/ValueArguments.java
@@ -1,4 +1,14 @@
 package ValueOrReferenceArguments;
 
 public class ValueArguments {
+    public static void main(String[] args) {
+
+        int i = 10;
+        System.out.println("We initialize main with i = " + i);
+        test(i);
+    }
+
+    public static void test (int i){
+        System.out.println("We initialize test method with i = " + i);
+    }
 }
