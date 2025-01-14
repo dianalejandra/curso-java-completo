8f4391aa8396fa5f325b2032b0103d1b1019df45
Diana Abate <diana.abate@gmail.com>
2025-01-14 11:31:49 -0300

    explanation of passing reference arguments

--- src/ValueOrReferenceArguments/PassAsReference.java
+++ src/ValueOrReferenceArguments/PassAsReference.java
@@ -2,16 +2,25 @@ package ValueOrReferenceArguments;
 
 public class PassAsReference {
     public static void main(String[] args) {
-
-        Integer i = 10;
-        System.out.println("We initialize main with i = " + i);
-        test(i);
-        System.out.println("Ending the execution of main method with i = " + i);
+        int [] age = {10,11,12};
+        System.out.println("We initialize main");
+        for(int i = 0; i < age.length; i++){
+            System.out.println("age[i] = " + age[i]);
+        }
+        System.out.println("Before calling test method");
+        test(age);
+        System.out.println("After calling test method");
+        for(int i = 0; i < age.length; i++){
+            System.out.println("age[i] = " + age[i]);
+        }
+        System.out.println("Ending the execution of main with the modified data of the array");
     }
 
-    public static void test(Integer i) {
-        System.out.println("We initialize test method with i = " + i);
-        i = 35;
-        System.out.println("Ending the execution of test method with i = " + i);
+    public static void test(int [] ageArr) {
+        System.out.println("We initialize test method");
+        for(int i = 0; i < ageArr.length; i++){
+            ageArr[i] = ageArr[i] + 20;
+        }
+        System.out.println("Ending the execution of test method" );
     }
 }
