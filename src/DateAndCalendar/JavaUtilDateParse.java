5046b87bda5bdbc051472a3d9848561b33019197
Diana <diana.abate@gmail.com>
2025-01-21 09:45:07 -0300

    working on explanation in JavaUtilDateParse

--- src/DateAndCalendar/JavaUtilDateParse.java
+++ src/DateAndCalendar/JavaUtilDateParse.java
@@ -8,7 +8,7 @@ public class JavaUtilDateParse {
     public static void main(String[] args) {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         try {
-            Date date = format.parse("2020-01-25");
+            Date date = format.parse("2020-12-25");
             System.out.println("date = " + date);
             System.out.println("format = " + format.format(date));
         } catch (ParseException e) {
