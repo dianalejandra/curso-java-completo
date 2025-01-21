4ee05ce546a21e7d499422f5a3a5e16f181568fe
Diana <diana.abate@gmail.com>
2025-01-21 09:49:37 -0300

    working on explanation in JavaUtilDateParse

--- src/DateAndCalendar/JavaUtilDateParse.java
+++ src/DateAndCalendar/JavaUtilDateParse.java
@@ -3,12 +3,17 @@ package DateAndCalendar;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class JavaUtilDateParse {
     public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        System.out.println("Add a date with format 'yyyy-MM-dd': ");
         try {
-            Date date = format.parse("2020-12-25");
+            Date date = format.parse(keyboard.next());
             System.out.println("date = " + date);
             System.out.println("format = " + format.format(date));
         } catch (ParseException e) {
