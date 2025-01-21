3858cd26c195c8146a353bbac903f1ae89c86499
Diana <diana.abate@gmail.com>
2025-01-21 10:38:08 -0300

    working with date comparisons

--- src/DateAndCalendar/JavaUtilDateParse.java
+++ src/DateAndCalendar/JavaUtilDateParse.java
@@ -16,6 +16,26 @@ public class JavaUtilDateParse {
             Date date = format.parse(keyboard.next());
             System.out.println("date = " + date);
             System.out.println("format = " + format.format(date));
+
+            Date actualDate = new Date();
+            System.out.println("actualDate = " + actualDate);
+
+            if (date.after(actualDate)){
+                System.out.println("Date inserted " + format.format(date) + " happens after " + actualDate);
+            } else if (date.before(actualDate)) {
+                System.out.println("Date inserted " + format.format(date) + " happens before " + actualDate);
+            } else if (date.equals(actualDate)) {
+                System.out.println("Both dates are the same");
+            }
+
+            if(date.compareTo(actualDate) > 0){
+                System.out.println("Date inserted " + format.format(date) + " happens after " + actualDate);
+            } else if (date.compareTo(actualDate) < 0) {
+                System.out.println("Date inserted " + format.format(date) + " happens before " + actualDate);
+            } else if (date.compareTo(actualDate) == 0) {
+                System.out.println("Both dates are the same");
+            }
+
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
