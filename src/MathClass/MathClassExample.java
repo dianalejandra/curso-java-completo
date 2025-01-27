e1fdfa8d4c40da4bd13634024d5471f4398258f8
Diana <diana.abate@gmail.com>
2025-01-27 11:06:28 -0300

    math class explanation

--- src/MathClass/MathClassExample.java
+++ src/MathClass/MathClassExample.java
@@ -26,6 +26,32 @@ public class MathClassExample {
         long round = Math.round(Math.PI);
         System.out.println("round = " + round);
 
+        double exp = Math.exp(2);
+        System.out.println("exp = " + exp);
 
+        double log = Math.log(10);
+        System.out.println("log = " + log);
+
+        double pow = Math.pow(10,3);
+        System.out.println("pow = " + pow);
+
+        double sqrt = Math.sqrt(5);
+        System.out.println("sqrt = " + sqrt);
+
+        double degrees = Math.toDegrees(1.57);
+        degrees = Math.round(degrees);
+        System.out.println("convert radians to degrees = " + degrees);
+
+        double radians = Math.toRadians(90);
+        System.out.println("convert degrees to radians = " + radians);
+
+        System.out.println("sin(90) = " + Math.sin(radians));
+        System.out.println("cos(90) = " + Math.cos(radians));
+
+        radians = Math.toRadians(180.00);
+        System.out.println("cos(180) = " + Math.cos(radians));
+
+        radians = Math.toRadians(0.00);
+        System.out.println("cos(180) = " + Math.cos(radians));
     }
 }
