77c84236774089e61df62a5ffd177b58e973f0da
Diana <diana.abate@gmail.com>
2025-01-27 10:12:53 -0300

    starting with a math class explanation

---
+++ src/MathClass/MathClassExample.java
@@ -0,0 +1,25 @@
+package MathClass;
+
+public class MathClassExample {
+    public static void main(String[] args) {
+
+        int absolute = Math.abs(-3);
+        System.out.println("absolute = " + absolute);
+
+        absolute = Math.abs(3);
+        System.out.println("absolute = " + absolute);
+
+        double max = Math.max(3.5,1.2);
+        System.out.println("max = " + max);
+
+        double min = Math.min(3.5,1.2);
+        System.out.println("min = " + min);
+
+        double ceil = Math.ceil(3.5);
+        System.out.println("ceil = " + ceil);
+        double floor = Math.floor(3.5);
+        System.out.println("floor = " + floor);
+        
+
+    }
+}
