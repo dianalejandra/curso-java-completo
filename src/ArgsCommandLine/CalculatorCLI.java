8f3795e62c8cd64c28e90926cddadee5791e9974
Diana <diana.abate@gmail.com>
2025-02-03 17:54:35 -0300

    adding error handling on the calculator example

--- src/ArgsCommandLine/CalculatorCLI.java
+++ src/ArgsCommandLine/CalculatorCLI.java
@@ -3,13 +3,20 @@ package ArgsCommandLine;
 public class CalculatorCLI {
     public static void main(String[] args) {
         if (args.length != 3) {
-            System.out.println("Usage: CalculatorCLI <operation: add, sub, mul, div> <nr a> <nr b>");
+            System.err.println("Usage: CalculatorCLI <operation: add, sub, mul, div> <nr a> <nr b>");
             System.exit(-1);
         }
         String operation = args[0];
-        int a = Integer.parseInt(args[1]);
-        int b = Integer.parseInt(args[2]);
+        int a = 0;
+        int b = 0;
         double result = 0.00;
+        try {
+            a = Integer.parseInt(args[1]);
+            b = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            System.err.println("Arguments should be numbers");
+            System.exit(-1);
+        }
         switch (operation) {
             case "add":
                 result = a + b;
@@ -22,7 +29,7 @@ public class CalculatorCLI {
                 break;
             case "div":
                 if (b == 0) {
-                    System.out.println("Division by zero is not allowed");
+                    System.err.println("Division by zero is not allowed");
                     System.exit(-1);
                 }
                 result = (double) a / b;
